91dd5ab79e3270a8b401abe57e6a1a999c5bd7f7
Bartek <bartek.polomski@gmail.com>
2018-09-11 23:31:02 +0200

    master Initial project with slow microservice and client microservice

---
+++ slow-service/src/main/java/complatablefuture/slowservice/SlowServiceApplication.java
@@ -0,0 +1,12 @@
+package complatablefuture.slowservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SlowServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SlowServiceApplication.class, args);
+	}
+}
---
+++ slow-service/src/test/java/complatablefuture/slowservice/SlowServiceApplicationTests.java
@@ -0,0 +1,16 @@
+package complatablefuture.slowservice;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class SlowServiceApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
